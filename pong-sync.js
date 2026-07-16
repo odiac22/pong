332a7d75232079ada683fe0e33ebe40a86bd7d92
Arian Jahandarfard <arianjahandarfard@gmail.com>
2026-07-16 13:18:52 -0500

    Track Random 40 first video timing

--- pong-sync.js
+++ pong-sync.js
@@ -5027,7 +5027,7 @@
     const accuracyText = document.createElement('span');
     accuracyText.id = 'random40-accuracy-mini';
     accuracyText.className = 'side-save-accuracy';
-    accuracyText.textContent = 'Nano --\nLocal --';
+    accuracyText.textContent = 'Nano --\nLocal --\nTime\nNano --\nLocal --';
 
     let videoHoldTimer = null;
     let videoLongPress = false;
